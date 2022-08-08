b17ae9c8eb20b77d3c123aa3ca4a9cb16c3c8213
Julia <tony_montana_07@mail.ru>
2022-08-08 21:15:46 +0400

    Решение первой задачи

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+            int firstnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int secondnum = Convert.ToInt32(Console.ReadLine());
+ 
+            if(firstnum > secondnum)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            else if (firstnum < secondnum)
+            {
+                Console.WriteLine("Первое число меньше второго");
+            }
