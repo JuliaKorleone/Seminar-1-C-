e84d9e1028827ddd9a3e61eeac9509e9390649d8
Julia <tony_montana_07@mail.ru>
2022-08-08 21:49:09 +0400

    Решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿//Первая задача
+Console.WriteLine("Введите первое число: ");
             int firstnum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число: ");
             int secondnum = Convert.ToInt32(Console.ReadLine());
@@ -12,6 +13,8 @@
                 Console.WriteLine("Первое число меньше второго");
             }
 
+
+//Вторая задача
 Console.WriteLine("Введите первое число: ");
             int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число: ");
@@ -30,4 +33,16 @@ Console.WriteLine("Введите первое число: ");
     else
     {
         Console.WriteLine("Второе число является максимальным ");
+    }
+
+// Третья задача
+    Console.WriteLine("Введите число: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            if (d % 2 == 0)
+            {
+                Console.WriteLine("Число является четным ");
+            }
+    else
+    {
+        Console.WriteLine("Число является нечетным ");
     }
